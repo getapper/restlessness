1c1f1880faf7fb2d470458fcf1afc594b957342a
Antonio <antonio.giordano@getapper.com>
2020-06-08 20:10:15 +0200

    MongoDao package tests init

--- packages/daos/mongo/src/index.ts
+++ packages/daos/mongo/src/index.ts
@@ -1,7 +1,7 @@
 import MongoBase from './base-model';
 import mongoDao from './dao';
 import { ObjectId } from 'mongodb';
-import { DaoPackage, JsonDaos } from '@restlessness/utilities';
+import { DaoPackage, JsonDaos, JsonEnvs, EnvFile } from '@restlessness/utilities';
 
 export * from './hooks';
 export * from './templates';
@@ -18,14 +18,21 @@ class MongoDaoPackage extends DaoPackage {
       name: 'Mongo DAO',
       package: '@restlessness/dao-mongo',
     });
-    // @TODO: Add RLN URI to each env
+    await JsonEnvs.read();
+    await Promise.all(JsonEnvs.entries.map(async jsonEnvsEntry => {
+      const envFile = new EnvFile(jsonEnvsEntry.id);
+      await envFile.setValue('RLN_MONGO_DAO_URI', `{RLN_MONGO_DAO_URI_${jsonEnvsEntry.id.toUpperCase()}}`);
+    }));
   }
 
   async postEnvCreated(envName: string): Promise<void> {
-    // @TODO: Add RLN URI to env
+    const envFile = new EnvFile(envName);
+    await envFile.setValue('RLN_MONGO_DAO_URI', `{RLN_MONGO_DAO_URI_${envName.toUpperCase()}}`);
   }
 
   async beforeLambda(): Promise<void> {
     // @TODO: open connection
   }
 }
+
+export default new MongoDaoPackage();
