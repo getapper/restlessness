48c7e80c7a48c46cd1b941bc675aa315cb977fb6
Antonio <antonio.giordano@getapper.com>
2020-06-09 19:16:44 +0200

    CLI Fixed copy script

--- packages/restlessness-cli/src/commands/dev/index.ts
+++ packages/restlessness-cli/src/commands/dev/index.ts
@@ -8,8 +8,8 @@ export default async (argv: minimist.ParsedArgs) => {
   if (majorVersion < 12) {
     throw new Error('Run command requires node version >= 12.x');
   }
-  
-  const backend = spawn('serverless', ['offline', '--port', "4123"], {
+
+  const backend = spawn('serverless', ['offline', '--port', '4123'], {
     cwd: path.join(__dirname, '..', '..', 'assets', 'backend'),
     env: {
       ...process.env,
