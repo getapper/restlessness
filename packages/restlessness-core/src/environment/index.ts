d6801cc128cb8eca883238a066ba38c8ce9d1b90
Andrea Santu <andreasantu@gmail.com>
2020-06-09 14:49:44 +0200

    rln-core load environment function

--- packages/restlessness-core/src/environment/index.ts
+++ packages/restlessness-core/src/environment/index.ts
@@ -1,10 +1,24 @@
 import path from 'path';
+import { config } from 'dotenv';
 
 const getEnvironmentName = () :string => {
   const env = require(path.join(process.cwd(), 'env.json'));
   return env.name;
 };
 
+function getLoader() {
+  let loaded = false;
+  return () => {
+    if (!loaded) {
+      config({ path: path.join(process.cwd(), '.env') });
+      loaded = true;
+    }
+  };
+}
+
+const loadEnv = getLoader();
+
 export {
   getEnvironmentName,
+  loadEnv,
 };
