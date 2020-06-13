025a844e5d852b20f819b91774532f217f96fd4c
Andrea Santu <andreasantu@gmail.com>
2020-06-13 16:04:46 +0200

    rln-cli: moved node version check on cli root

--- packages/restlessness-cli/src/index.ts
+++ packages/restlessness-cli/src/index.ts
@@ -7,6 +7,11 @@ import dev from './commands/dev';
 import createEnv from './commands/create-env';
 
 const cli = async () => {
+  const majorVersion: number = parseInt((/^(\d+)(\.\d+){0,2}$/.exec(process.versions.node))[1], 10);
+  if (majorVersion < 12) {
+    throw new Error('Run command requires node version >= 12.x');
+  }
+
   const argv = minimist(process.argv.slice(2));
   switch(argv._[0]) {
     case 'new':
