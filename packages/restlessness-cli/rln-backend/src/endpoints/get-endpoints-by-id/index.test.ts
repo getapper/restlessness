b16811722ca53f028d857d29068a5d0ee89f8d0f
antoniogiordano <antonio.giordano@getapper.com>
2020-06-16 18:40:57 +0200

    New releases

--- packages/restlessness-cli/rln-backend/src/endpoints/get-endpoints-by-id/index.test.ts
+++ packages/restlessness-cli/rln-backend/src/endpoints/get-endpoints-by-id/index.test.ts
@@ -1,9 +1,9 @@
-import { StatusCodes, apiCall } from '@restlessness/core';
+import { StatusCodes, TestHandler } from '@restlessness/core';
 
 const getEndpointsById = 'getEndpointsById';
 
 test('', async (done) => {
-  const res = await apiCall(getEndpointsById);
+  const res = await TestHandler.invokeLambda(getEndpointsById);
   // expect(res.statusCode).toBe(StatusCodes.OK);
   done();
 });
