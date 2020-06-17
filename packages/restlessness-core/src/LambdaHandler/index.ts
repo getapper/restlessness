75bec2ad26e9860d47ecbec910df2cae971cf3df
Andrea Santu <andreasantu@gmail.com>
2020-06-17 10:41:45 +0200

    rln-core: check Dao beforeLambda hook

--- packages/restlessness-core/src/LambdaHandler/index.ts
+++ packages/restlessness-core/src/LambdaHandler/index.ts
@@ -1,10 +1,13 @@
 import { ValidationObjects, ValidationResult, RequestI } from './interfaces';
+import { JsonEndpoints, DaoPackage, JsonDaos, JsonDaosEntry } from '@restlessness/utilities';
 import AWSLambda from 'aws-lambda';
+
 export * from './interfaces';
 
 export const LambdaHandler = async <T, Q, P, PP>(
   handler: (req: RequestI<Q, P, PP>) => any,
   validations: ValidationObjects,
+  apiName: string,
   event: AWSLambda.APIGatewayProxyEventBase<T>,
   context: AWSLambda.Context,
 ) => {
@@ -46,7 +49,23 @@ export const LambdaHandler = async <T, Q, P, PP>(
     }
   }
 
-  // @TODO: Check DAOs and Plugins beforeLambdas hooks
+  // @TODO: Check Plugins beforeLambdas hooks
+  const jsonEndpointsEntry = await JsonEndpoints.getEntryById(apiName);
+  if (jsonEndpointsEntry) {
+    if (jsonEndpointsEntry.daoIds?.length) {
+      for (const daoId of jsonEndpointsEntry.daoIds)  {
+        const jsonDaoEntry: JsonDaosEntry = await JsonDaos.getEntryById(daoId);
+        try {
+          const daoPackage: DaoPackage = DaoPackage.load(jsonDaoEntry.package);
+          await daoPackage.beforeLambda(event, context);
+        } catch (e) {
+          console.error(`Error when calling beforeLambda hook on dao: ${jsonDaoEntry.name} (${jsonDaoEntry.id})`, e);
+        }
+      }
+    }
+  } else {
+    console.error(`Cannot find Endpoint identified by ${apiName}`);
+  }
 
   return await handler({
     validationResult,
