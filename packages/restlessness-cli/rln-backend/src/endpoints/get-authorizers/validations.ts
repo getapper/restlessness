5ce3a4cd06907296e89a3b3439f285ccf5f10ae4
lucatedesco <luca.tedesco@m.getapper.com>
2020-06-18 16:10:44 +0200

    fixed rln-backend internal validations and models/openApi with latest change for validations structure.

--- packages/restlessness-cli/rln-backend/src/endpoints/get-authorizers/validations.ts
+++ packages/restlessness-cli/rln-backend/src/endpoints/get-authorizers/validations.ts
@@ -2,9 +2,9 @@ import * as yup from 'yup';
 import { QueryStringParameters } from './interfaces';
 import { YupShapeByInterface } from '@restlessness/core';
 
-const queryStringParametersValidations: YupShapeByInterface<QueryStringParameters> = {};
+const queryStringParametersValidations = () : YupShapeByInterface<QueryStringParameters> => ({});
 
-export default {
-  queryStringParameters: yup.object().shape(queryStringParametersValidations),
-};
+export default () => ({
+  queryStringParameters: yup.object().shape(queryStringParametersValidations()),
+});
 
