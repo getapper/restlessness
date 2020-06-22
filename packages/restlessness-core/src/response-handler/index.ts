b060323c9f51769ed65b6937b36bea8587bcb620
Andrea Santu <andreasantu@gmail.com>
2020-06-22 11:51:10 +0200

    Issue #33: refactor res function to ResponseHandler class

--- packages/restlessness-core/src/response-handler/index.ts
+++ packages/restlessness-core/src/response-handler/index.ts
@@ -6,23 +6,25 @@ import {
 } from './interfaces';
 import path from 'path';
 
-const res = (response, statusCode:StatusCodes = StatusCodes.OK, options?: ResponseOptions): Response => {
-  const headers: HttpHeader = {};
-  try {
-    const defaultHeaders = require(path.join(process.cwd(), 'default-headers.json'));
-    Object.assign(headers, defaultHeaders);
-  } catch (e) {}
-  if (options?.headers) {
-    Object.assign(headers, options?.headers);
+class ResponseHandler {
+  static json (response, statusCode: StatusCodes = StatusCodes.OK, options?: ResponseOptions): Response {
+    const headers: HttpHeader = {};
+    try {
+      const defaultHeaders = require(path.join(process.cwd(), 'default-headers.json'));
+      Object.assign(headers, defaultHeaders);
+    } catch (e) {}
+    if (options?.headers) {
+      Object.assign(headers, options?.headers);
+    }
+    return {
+      statusCode,
+      body: JSON.stringify(response),
+      headers,
+    };
   }
-  return {
-    statusCode,
-    body: JSON.stringify(response),
-    headers,
-  };
-};
+}
 
 export {
-  res,
+  ResponseHandler,
 };
 export * from './interfaces';
