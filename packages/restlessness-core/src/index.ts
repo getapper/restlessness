5b6c0a5edc1f67b3e287f5eb3234d1ae8a7a8f1a
Andrea Santu <andreasantu@gmail.com>
2020-06-30 17:41:54 +0200

    rln-core: LambdaAuthorizerHandler, generatePolicy

--- packages/restlessness-core/src/index.ts
+++ packages/restlessness-core/src/index.ts
@@ -1,4 +1,5 @@
 export * from './LambdaHandler';
+export * from './LambdaAuthorizerHandler';
 export * from './response-handler';
 export { default as EnvironmentHandler } from './EnvironmentHandler';
 export * from './TestHandler';
