0869db128cfce3ce7a489765962a288d1b619cd7
antoniogiordano <antoniogiordano@getapper.com>
2020-10-21 16:58:05 +0200

    CLI: Removed olf frontend

--- packages/restlessness-cli/rln-frontend/src/swagger/list/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SwaggerUI from "swagger-ui-react";
-import "swagger-ui-react/swagger-ui.css"
-
-const SwaggerList = (props) => (
-  <SwaggerUI url={`http://localhost:4123/openapi`} />
-);
-
-export default SwaggerList;
