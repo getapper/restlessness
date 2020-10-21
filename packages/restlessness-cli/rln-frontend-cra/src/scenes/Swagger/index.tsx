2c1b2eb874d545f1a55130c6bf726fd99ec4f823
antoniogiordano <antoniogiordano@getapper.com>
2020-10-21 16:56:48 +0200

    CLI: Fixed openapi URL in frontend

--- packages/restlessness-cli/rln-frontend-cra/src/scenes/Swagger/index.tsx
+++ packages/restlessness-cli/rln-frontend-cra/src/scenes/Swagger/index.tsx
@@ -2,7 +2,7 @@ import React, { memo } from "react";
 import SwaggerUI from "swagger-ui-react";
 
 const Swagger = () => {
-  return <SwaggerUI url="http://localhost:4000/openapi" />;
+  return <SwaggerUI url="http://localhost:4000/dev/openapi" />;
 };
 
 export default memo(Swagger);
