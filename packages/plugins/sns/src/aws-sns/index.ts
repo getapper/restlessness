2bdb580eb9bc8432d120ae651aa743c0e8dfe4ec
Zhin1797 <79398568+Zhin1797@users.noreply.github.com>
2021-04-30 15:43:54 +0200

    - Removed comment

--- packages/plugins/sns/src/aws-sns/index.ts
+++ packages/plugins/sns/src/aws-sns/index.ts
@@ -11,37 +11,6 @@ class AwsSNS {
     this.awsSNS = new SNS(SNSConfigOptions);
   }
 
-  /*
-  async sendEmail(
-    sender: string,
-    receivers: string[],
-    subject: string,
-    content: string,
-    awsSendEmailParams?: SES.Types.SendEmailRequest,
-  ): Promise<SES.Types.SendEmailResponse> {
-    const params: SES.Types.SendEmailRequest = {
-      Source: sender,
-      Destination: {
-        ToAddresses: receivers,
-      },
-      Message: {
-        Subject: {
-          Charset: 'UTF-8',
-          Data: subject,
-        },
-        Body: {
-          Html: {
-            Charset: 'UTF-8',
-            Data: content,
-          },
-        },
-      },
-      ...awsSendEmailParams,
-    };
-    return await this.awsSES.sendEmail(params).promise();
-  }
-   */
-
   async createTopic(topicName: string, displayName: string = topicName) {
     const params: SNS.Types.CreateTopicInput = {
       Name: topicName,
